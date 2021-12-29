c496a82123d9aef30af0d3e3e22c229a7d9628f1
Marius S <seffermarius@gmail.com>
2021-12-29 09:50:00 +0200

    initial project structure created

---
+++ src/main/java/se/kth/iv1351/project/Iv1351ProjectApplication.java
@@ -0,0 +1,13 @@
+package se.kth.iv1351.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Iv1351ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Iv1351ProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/kth/iv1351/project/Iv1351ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package se.kth.iv1351.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Iv1351ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
