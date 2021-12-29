de7f33396d49288db14e9db0589a46a5a0a536e0
Marius S <seffermarius@gmail.com>
2021-12-29 16:37:19 +0200

    Mybatis dependency + structure created

---
+++ src/main/java/se/kth/iv1351/model/User.java
@@ -0,0 +1,55 @@
+package se.kth.iv1351.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class User {
+    private Integer id;
+
+    private String name;
+
+    private String personNumber;
+
+    private Integer age;
+
+    private String email;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPersonNumber() {
+        return personNumber;
+    }
+
+    public void setPersonNumber(String personNumber) {
+        this.personNumber = personNumber;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
