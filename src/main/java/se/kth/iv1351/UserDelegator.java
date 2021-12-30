48a8b638c0b0ab09db8dd9688c556b7fd18e9d76
Marius S <seffermarius@gmail.com>
2021-12-30 15:24:36 +0200

    lombok added for cleaner code

--- src/main/java/se/kth/iv1351/UserDelegator.java
+++ src/main/java/se/kth/iv1351/UserDelegator.java
@@ -1,39 +1,24 @@
 package se.kth.iv1351;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.context.request.NativeWebRequest;
+import se.kth.iv1351.dao.UserMapper;
+import se.kth.iv1351.model.UserData;
 import se.kth.iv1351.openapi.UserApiDelegate;
 import se.kth.iv1351.openapi.model.User;
 
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-
+@AllArgsConstructor
 @Service
 public class UserDelegator implements UserApiDelegate {
-    @Autowired
-    private DataSource dataSource;
+    private UserMapper userMapper;
 
     @Override
     public ResponseEntity<User> userGet() {
-        Connection conn = null;
-        try {
-            conn = dataSource.getConnection();
-            PreparedStatement stmt = conn.prepareStatement("SELECT count(1) FROM users");
-            ResultSet res = stmt.executeQuery();
-
-            User user = new User();
-            user.setName("Fisen");
-            return new ResponseEntity<User>(user, HttpStatus.OK);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        UserData userData = userMapper.selectUser();
+        User user  = new User();
+        user.setName("Fisen");
+        return new ResponseEntity<User>(user, HttpStatus.OK);
     }
 }
