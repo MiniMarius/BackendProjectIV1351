409e66a37e8fe612a2c23990ed444fc02437b7eb
Marius S <seffermarius@gmail.com>
2021-12-31 10:37:07 +0200

    added initial models for ORM later

--- src/main/java/se/kth/iv1351/model/UserData.java
+++ src/main/java/se/kth/iv1351/model/UserData.java
@@ -1,7 +1,10 @@
 package se.kth.iv1351.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class UserData {
     private Integer id;
 
@@ -12,44 +15,4 @@ public class UserData {
     private Integer age;
 
     private String email;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPersonNumber() {
-        return personNumber;
-    }
-
-    public void setPersonNumber(String personNumber) {
-        this.personNumber = personNumber;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
