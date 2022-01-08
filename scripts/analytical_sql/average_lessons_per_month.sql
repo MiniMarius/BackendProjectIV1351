22c8f894a53fa78378f6865d74a0827c3e9ffa8c
Marius S <seffermarius@gmail.com>
2022-01-08 15:57:51 +0200

    typo fix

--- scripts/analytical_sql/average_lessons_per_month.sql
+++ scripts/analytical_sql/average_lessons_per_month.sql
@@ -9,7 +9,7 @@ FROM
     SELECT
         extract(month from cast(bookings.start_time as date)) AS start_month
         , count(distinct bookings.id) AS total_lessons_held
-        , count(distinct CASE WHEN lessons.type = 'individual' THEN bookings.id ELSE NULL END) AS total_individiual_lessons_held
+        , count(distinct CASE WHEN lessons.type = 'individual' THEN bookings.id ELSE NULL END) AS total_individual_lessons_held
         , count(distinct CASE WHEN lessons.type = 'ensemble' THEN bookings.id ELSE NULL END) AS total_ensemble_lessons_held
         , count(distinct CASE WHEN lessons.type = 'group' THEN bookings.id ELSE NULL END) AS total_group_lessons_held
     FROM
