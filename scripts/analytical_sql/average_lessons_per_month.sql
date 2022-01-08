edc40e5f5b3c1c43f6a2a3473142db54f9d8566c
Marius S <seffermarius@gmail.com>
2022-01-08 16:11:10 +0200

    query fix

--- scripts/analytical_sql/average_lessons_per_month.sql
+++ scripts/analytical_sql/average_lessons_per_month.sql
@@ -1,15 +1,14 @@
 SELECT
-    avg(total_lessons_held) AS avg_total_lessons_held
-    , avg(total_individiual_lessons_held) AS avg_total_individiual_lessons_held
-    , avg(total_ensemble_lessons_held) AS avg_total_ensemble_lessons_held
-    , avg(total_group_lessons_held) AS avg_total_group_lessons_held
+    total_lessons_held / 12.0 AS avg_total_lessons_held
+    , total_individiual_lessons_held / 12.0 AS avg_total_individiual_lessons_held
+    , total_ensemble_lessons_held / 12.0 AS avg_total_ensemble_lessons_held
+    , total_group_lessons_held / 12.0 AS avg_total_group_lessons_held
 
 FROM
 (
     SELECT
-        extract(month from cast(bookings.start_time as date)) AS start_month
-        , count(distinct bookings.id) AS total_lessons_held
-        , count(distinct CASE WHEN lessons.type = 'individual' THEN bookings.id ELSE NULL END) AS total_individual_lessons_held
+        count(distinct bookings.id) AS total_lessons_held
+        , count(distinct CASE WHEN lessons.type = 'individual' THEN bookings.id ELSE NULL END) AS total_individiual_lessons_held
         , count(distinct CASE WHEN lessons.type = 'ensemble' THEN bookings.id ELSE NULL END) AS total_ensemble_lessons_held
         , count(distinct CASE WHEN lessons.type = 'group' THEN bookings.id ELSE NULL END) AS total_group_lessons_held
     FROM
@@ -18,7 +17,6 @@ FROM
         lessons
     ON bookings.lesson_id = lessons.id
     WHERE
-        extract(year from cast(bookings.start_time as date)) = 2022
-    GROUP BY
-        1
+        extract(year from cast(bookings.start_time as date)) = #{input_year}
 ) as a;
+
